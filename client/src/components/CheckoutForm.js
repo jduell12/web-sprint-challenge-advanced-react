f34747c22a9ffaaae7b42d46cdb3f168ff346859
Jessica <duellje@oregonstate.edu>
2020-07-10 11:23:27 -0400

    created custom hook for form

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import {useForm} from '../hooks/useForm'
 
-const initialValue = {
+const initialValues = {
   firstName: "",
   lastName: "",
   address: "",
@@ -15,11 +16,9 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const useFormHook = useForm(initialValues);
+  const [values, handleChanges] = useFormHook;
 
   const handleSubmit = (e) => {
     e.preventDefault();
